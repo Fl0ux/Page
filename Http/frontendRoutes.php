f6dd4804c6cf1137ce82e294838325388987af11
Foulques du Peloux <foulques@dupeloux.fr>
2016-04-26 12:00:54 +0200

    Allow to define custom URLs
    This change allow to define custom URLs for a page. For example you can now define this type of URL for a page : foo/bar
    
    This can be very useful for SEO purpose.

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -4,6 +4,6 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 if (! App::runningInConsole()) {
-    $router->get('/', ['uses' => 'PublicController@homepage', 'as' => 'homepage']);
+    $router->get('/', ['uses' => 'PublicController@homepage', 'as' => 'homepage'])->where('uri', '.*');
     $router->any('{uri}', ['uses' => 'PublicController@uri', 'as' => 'page'])->where('uri', '.*');
 }
